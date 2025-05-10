023999c47a072cff260288b90cf8542a184debf0
Shereen Mohamed <163689317+Shereen-Mohamed36@users.noreply.github.com>
2025-05-10 01:31:34 -0700

    Update old path with zakat.css path

--- src/Views/zakatView.php
+++ src/Views/zakatView.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Zakat Calculator</title>
-    <link rel="stylesheet" href="../zakat-calculator.css">
+    <link rel="stylesheet" href="../zakat.css">
 </head>
 <body>
 <div class="container">
